749c50f550f5121592ad6b96f04891e973696bb3
Murilo Antunes <mjantunes09@gmail.com>
2021-11-24 13:57:16 -0300

    erro-nas-escolhas-arrumado

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,10 +1,13 @@
 function redirecionarParaFase() {
   var caminho_1 = document.getElementById("caminho_1").value;
   var caminho_2 = document.getElementById("caminho_2").value;
-
-  var numEscolha = parseInt(prompt(`Escolha a opção 1 ou 2:`));
+  var numEscolha;
 
   while (numEscolha != 1 || numEscolha != 2) {
+    numEscolha = parseInt(prompt(`Escolha a opção 1 ou 2:`));
+    if (numEscolha === null) {
+      return;
+    }
     if (numEscolha == 1) {
       window.location.assign(caminho_1);
       break;
@@ -13,10 +16,6 @@ function redirecionarParaFase() {
       break;
     } else {
       alert("Escolha um caminho válido!");
-      numEscolha = parseInt(prompt(`Escolha a opção 1 ou 2:`));
-      if(input === null) {
-        return
-      }
-    } 
+    }
   }
 }
--- javaScript/javaScript_home.js
+++ javaScript/javaScript_home.js
@@ -14,15 +14,19 @@ function redirecionarParaFase(numEscolha) {
 
 function selecionarPersonagem(event) {
   event.preventDefault();
-  var escolha = parseInt(
-    prompt(`Escolha seu personagem:
-  1 - Jarom, o Sábio
-  2 - Hélio, o Explorador
-  3 - Esli, o Guerreiro
-  Obs: Escolha baseado no número do personagem!`)
-  );
+  var escolha;
 
   while (escolha != 1 || escolha != 2 || escolha != 3) {
+    escolha = parseInt(
+      prompt(`Escolha seu personagem:
+    1 - Jarom, o Sábio
+    2 - Hélio, o Explorador
+    3 - Esli, o Guerreiro
+    Obs: Escolha baseado no número do personagem!`)
+    );
+    if (escolha === null) {
+      return;
+    }
     if (escolha == 1) {
       alert("Você escolheu Jarom, o Sábio!");
       redirecionarParaFase(escolha);
@@ -37,15 +41,6 @@ function selecionarPersonagem(event) {
       break;
     } else {
       alert("Escolha um personagem válido!");
-      escolha = parseInt(
-        prompt(`Escolha seu personagem:
-  1 - Jarom, o Sábio
-  2 - Hélio, o Explorador
-  3 - Esli, o Guerreiro
-  Obs: Escolha baseado no número do personagem!`));
-      if(input === null) {
-        return
-      }
     }
   }
 }
